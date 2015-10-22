31f87017e5fab01e408d0691cf4b52059f1613bb
yush ch <yschen9218@gmail.com>
2015-10-22 04:18:24 +0000

    week2 implemented in ruby, wasn't able to get 3rd right

---
+++ algo/quicksort.rb
@@ -0,0 +1,95 @@
+class QuickSort
+  attr_accessor :comparisons, :array
+  
+  def initialize
+    @comparisons = 0
+    @array = array_set #读数组进去
+    #test_read
+  end
+  
+  def array_set
+    array = []
+    File.open("QuickSort.txt", "r") do |f|
+      f.each_line do |num|
+        array << num.to_i
+        #break if array.length == 3
+      end
+    end
+    return array
+  end
+  
+  def quick_sort(array)
+    if array.length <= 1
+      #@comparisons += 1 if array.length == 1
+      return array
+    end
+    puts ""
+    
+    print "array now is #{array}"
+    
+    puts ""
+    
+    p = get_pivot(array)
+    
+    puts "the pivot is #{p}"
+    
+    partition_array = partition(array, p)
+    index = partition_array.index(p)
+    array_first = []
+    array_first = partition_array[0..(index-1)] if (index - 1) >= 0
+    @comparisons += array_first.length if array_first.length >= 1
+    array_second = []
+    array_second = partition_array[(index+1)..(partition_array.length-1)] if (index+1 <= partition_array.length - 1)
+    @comparisons += array_second.length if array_second.length >= 1
+
+    print "comparison now is #{@comparisons}" + "\n"
+    print "\n"
+    print array_first
+    print "     "
+    print array_second
+    puts ""
+
+    #return("false") if @comparisons == 29
+    return quick_sort(array_first) + [p] + quick_sort(array_second)
+  end
+  
+  def partition(array, p)
+    #@comparisons += 1 if array.length == 1
+    return array if array.length <= 1
+    #@comparisons += array.length - 2
+    
+    index = array.index(p)
+    #@comparisons += 1 if index != 0
+    array[0], array[index] = array[index], array[0] #if index != 0 #swap the 2 element
+    i = 1
+    #@comparisons += array.length - 2
+    for index in (1..(array.length - 1))
+      if array[index] < p
+        array[i], array[index] = array[index], array[i]
+        i += 1
+      end 
+    end
+    array[0], array[i-1] = array[i-1], array[0]
+    return array
+  end
+  
+  def get_pivot(array)
+    #return array.first
+    #return array.last
+    return array[(array.length)/2] if array.length % 2 == 1
+    return array[(array.length)/2 - 1]
+    
+  end
+  
+  def test_read#this si just for test
+    puts @array
+  end
+  
+end
+
+q =QuickSort.new
+#q.quick_sort(q.array)
+q.quick_sort([3,9,8,4,6,10,2,5,7,1])
+puts ""
+print q.comparisons
+#print q.partition([9,14,13,12,11,10,8,7,6], 9)
